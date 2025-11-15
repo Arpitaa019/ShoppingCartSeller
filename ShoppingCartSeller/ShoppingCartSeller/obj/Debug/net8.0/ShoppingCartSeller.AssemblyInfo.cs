b6580255105953e7f78b179a3bb517f110833639
Arpita <jaiswalarpita109@gmail.com>
2025-11-15 10:22:11 +0530

    commit

--- ShoppingCartSeller/ShoppingCartSeller/obj/Debug/net8.0/ShoppingCartSeller.AssemblyInfo.cs
+++ ShoppingCartSeller/ShoppingCartSeller/obj/Debug/net8.0/ShoppingCartSeller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCartSeller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bcca4605bcb77cd605ef28b411dfae9a7332c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897beec7f2a5a4aa46f4ed5ae3f4188d754ebd61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCartSeller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCartSeller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
